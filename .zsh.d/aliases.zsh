bed731f3bb5f3687de1fcb33576836073da47b8d
Joël Matelli <joel.matelli@codikos.io>
2022-10-26 15:39:21 +0200

    chore: misc updates on configs

--- .zsh.d/aliases.zsh
+++ .zsh.d/aliases.zsh
@@ -3,3 +3,5 @@ alias nvl='XDG_DATA_HOME=$NVIM_PATH/share XDG_CONFIG_HOME=$NVIM_PATH nvim'
 alias ls='exa -h --group-directories-first -1'
 alias l='exa -h --group-directories-first --icons'
 alias ll='exa -lah --group-directories-first --icons'
+
+alias dotf='cd ~/.dotfiles'
