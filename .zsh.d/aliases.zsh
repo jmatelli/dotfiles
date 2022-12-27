2acb8a5dad579d99dc0b588c6fadbca57efc4a3d
Joël Matelli <joel.matelli@codikos.io>
2022-12-27 10:02:28 +0100

    refactor(vim+git): improve vim mappings and add git aliases

--- .zsh.d/aliases.zsh
+++ .zsh.d/aliases.zsh
@@ -5,3 +5,18 @@ alias l='exa -h --group-directories-first --icons'
 alias ll='exa -lah --group-directories-first --icons'
 
 alias dotf='cd ~/.dotfiles'
+
+# Git
+alias gp='git pull'
+alias gco='git co'
+alias gnew='git co -b'
+alias gst='git st'
+alias gap='git add -p'
+alias gaa='git add -A'
+alias gcm='git cm'
+alias gcan='git can'
+alias gsta='git stash'
+alias gstu='git stash -u'
+alias gpu='git push'
+alias gbr='git br | fzf'
+alias gclean='git branch -d `git branch --merged | ag -v "\\*" | ag -v main | ag -v master | ag -v devel`'
--- nvim/lua/core/utils.lua
+++ nvim/lua/core/utils.lua
@@ -8,6 +8,9 @@ end
 M.vnoremap = function(lhs, rhs, options)
   keymap('v', lhs, rhs, options or { silent = true })
 end
+M.tnoremap = function(lhs, rhs, options)
+  keymap('t', lhs, rhs, options or { silent = true })
+end
 M.nleader = function(lhs, rhs, options)
   keymap('n', '<leader>' .. lhs, rhs, options or { silent = true })
 end
