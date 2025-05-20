f87a0fb4a51feb426e1ccffce78d47264ca2151b
Joël Matelli <jo.matelli@gmail.com>
2025-05-20 15:08:23 +0200

    fix: database lua

--- .config/nvim/lua/plugins/database.lua
+++ .config/nvim/lua/plugins/database.lua
@@ -14,67 +14,22 @@ return {
     local local_ip = vim.env.LOCAL_IP
     local urlprefix = "postgresql://postgres:postgres@" .. local_ip .. ":5432/"
 
-    local yaarzStg = {
-      readonly = {
-        username = vim.env.YAARZ_READONLY_STG_DB_USERNAME,
-        password = vim.env.YAARZ_READONLY_STG_DB_PASSWORD,
-      },
-      write = {
-        username = vim.env.YAARZ_STG_DB_USERNAME,
-        password = vim.env.YAARZ_STG_DB_PASSWORD,
-      },
-    }
-
-    local yaarzProd = {
-      readonly = {
-        username = vim.env.YAARZ_READONLY_STG_DB_USERNAME,
-        password = vim.env.YAARZ_READONLY_STG_DB_PASSWORD,
-      },
-      write = {
-        username = vim.env.YAARZ_STG_DB_USERNAME,
-        password = vim.env.YAARZ_STG_DB_PASSWORD,
-      },
-    }
-
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_use_nvim_notify = 1
+
+    local stg_read_url = vim.env.YAARZ_READONLY_STG_DB_URL
+    local stg_write_url = vim.env.YAARZ_STG_DB_URL
+    local prod_read_url = vim.env.YAARZ_PROD_READONLY_DB_URL
+    local prod_write_url = vim.env.YAARZ_PROD_DB_URL
 
     vim.g.dbs = {
       { name = "Yaarz - DEV", url = urlprefix .. "yaarz" },
       { name = "Yaarz - TEST", url = urlprefix .. "yaarz_test" },
       { name = "Yaarz - E2E TEST", url = urlprefix .. "yaarz_e2e_test" },
-      {
-        name = "Yaarz - STG readonly",
-        url = "postgresql://"
-          .. yaarzStg.readonly.username
-          .. ":"
-          .. yaarzStg.readonly.password
-          .. "@b2jjkkefpdifbb1skobz-postgresql.services.clever-cloud.com:6769/b2jjkkefpdifbb1skobz",
-      },
-      {
-        name = "Yaarz - STG",
-        url = "postgresql://"
-          .. yaarzStg.write.username
-          .. ":"
-          .. yaarzStg.write.password
-          .. "@b2jjkkefpdifbb1skobz-postgresql.services.clever-cloud.com:6769/b2jjkkefpdifbb1skobz",
-      },
-      {
-        name = "Yaarz - PROD readonly",
-        url = "postgresql://"
-          .. yaarzProd.readonly.username
-          .. ":"
-          .. yaarzProd.readonly.password
-          .. "@b2jjkkefpdifbb1skobz-postgresql.services.clever-cloud.com:6769/b2jjkkefpdifbb1skobz",
-      },
-      {
-        name = "Yaarz - PROD",
-        url = "postgresql://"
-          .. yaarzProd.write.username
-          .. ":"
-          .. yaarzProd.write.password
-          .. "@b2jjkkefpdifbb1skobz-postgresql.services.clever-cloud.com:6769/b2jjkkefpdifbb1skobz",
-      },
+      { name = "Yaarz - STG readonly", url = stg_read_url },
+      { name = "Yaarz - STG", url = stg_write_url },
+      { name = "Yaarz - PROD readonly", url = prod_read_url },
+      { name = "Yaarz - PROD", url = prod_write_url },
     }
   end,
   keys = {
